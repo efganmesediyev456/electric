211d55499bd2f50ebc6dc72a013dd1c85b9402ad
user <yadigarmemmedov1996@gmail,com>
2019-07-03 09:50:16 +0400

    sliderler duzeldi

--- resources/views/site/index/sliders.blade.php
+++ resources/views/site/index/sliders.blade.php
@@ -11,7 +11,8 @@
             <!-- Wrapper for slides -->
             <div class="carousel-inner" role="listbox">
                 <!-- Item 1 -->
-                <div class="item slide1 active">
+                @foreach($sliders as $key=>$slider)
+                <div class="item slide{{ $key }} active">
                     <div class="row">
                         <div class="container">
                             <div class="col-md-4 col-sm-5 text-center hidden-sm hidden-md hidden-lg"> <img data-animation="animated zoomInLeft" src="images/{{$sliders[0]->image}}" alt=""> </div>
@@ -20,38 +21,12 @@
                                 <p data-animation="animated bounceInLeft" class="">Lorem Ipsum is simply dummy text of the industry<span> industry's standard dummy text.</span></p>
                                 <a class="btn btn-hero" data-animation="animated bounceInLeft" href="contact.html">Contact Us</a>
                             </div>
-                            <div class="col-md-4 col-sm-5 text-right hidden-xs"> <img data-animation="animated zoomInLeft" src="images/{{$sliders[0]->image}}" alt=""> </div>
+                            <div class="col-md-4 col-sm-5 text-right hidden-xs"> <img data-animation="animated zoomInLeft" src="{{ route('home')}}/images/{{$slider->image}}" alt=""> </div>
                         </div>
                     </div>
                 </div>
-                <!-- Item 2 -->
-                <div class="item slide2">
-                    <div class="row">
-                        <div class="container">
-                            <div class="col-md-4 col-sm-5 text-center hidden-sm hidden-md hidden-lg"> <img data-animation="animated zoomInLeft" src="images/{{$sliders[1]->image}}" alt=""> </div>
-                            <div class="col-md-8 col-sm-7 text-left">
-                                <h1 data-animation="animated bounceInLeft" class="">Welcome To <span>Best Electrical Services</span></h1>
-                                <p data-animation="animated bounceInLeft" class="">Lorem Ipsum is simply dummy text of the industry<span> industry's standard dummy text.</span></p>
-                                <a class="btn btn-hero" data-animation="animated bounceInLeft" href="contact.html">Contact Us</a>
-                            </div>
-                            <div class="col-md-4 col-sm-5 text-right hidden-xs"> <img data-animation="animated zoomInLeft" src="images/{{$sliders[1]->image}}" alt=""> </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Item 3 -->
-                <div class="item slide3">
-                    <div class="row">
-                        <div class="container">
-                            <div class="col-md-4 col-sm-5 text-center hidden-sm hidden-md hidden-lg"> <img data-animation="animated zoomInLeft" src="images/slider-img1.png" alt=""> </div>
-                            <div class="col-md-8 col-sm-7 text-left">
-                                <h1 data-animation="animated bounceInLeft" class="">Welcome To <span>Best Electrical Services</span></h1>
-                                <p data-animation="animated bounceInLeft" class="">Lorem Ipsum is simply dummy text of the industry industry's standard dummy text.</p>
-                                <a class="btn btn-hero" data-animation="animated bounceInLeft" href="contact.html">Contact Us</a>
-                            </div>
-                            <div class="col-md-4 col-sm-5 text-right hidden-xs"> <img data-animation="animated zoomInLeft" src="images/slider-img3.png" alt=""> </div>
-                        </div>
-                    </div>
-                </div>
+                    @endforeach
+
             </div>
             <!-- End Wrapper for slides--> <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev"> <i class="fa fa-angle-left"></i><span class="sr-only">Previous</span> </a> <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next"> <i class="fa fa-angle-right"></i><span class="sr-only">Next</span> </a>
         </div>
